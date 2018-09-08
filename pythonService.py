4929e6211d8c48942bfa0222763ca6ff6823acc5
yongliangliu <liuyongliang123@gmail.com>
2018-09-08 12:00:22 +0800

    fix bugs

--- pythonService.py
+++ pythonService.py
@@ -151,7 +151,7 @@ def drawShareImgCar(appoint,key):
 
 
 def drawShareImgPassenger(appoint,key):
-    canvas = Image.open("./img/车找人.jpg")
+    canvas = Image.open("./img/人找车.jpg")
     tel=appoint['tel']
     fromName=appoint['from']['name']
     toName=appoint['to']['name']
