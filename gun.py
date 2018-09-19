c0cda19e0aa50ed9d2253bf82133de1b8a04b5ff
yongliangliu <liuyongliang123@gmail.com>
2018-09-19 23:51:42 +0800

    fix gun.py

--- gun.py
+++ gun.py
@@ -25,9 +25,10 @@ chdir = '/home/lyl/pythonService'
 daemon = True 
 
 #启动的进程数
-workers = multiprocessing.cpu_count() * 2 + 1 
-# worker_class = 'gunicorn.workers.ggevent.GeventWorker'
-worker_class = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
+workers = multiprocessing.cpu_count() * 2 + 1
+#worker_class = 'gunicorn.workers.ggevent.GeventWorker'
+#worker_class='geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
+worker_class = 'flask_sockets.worker'
 threads = 4
 
 x_forwarded_for_header = 'X-FORWARDED-FOR'
