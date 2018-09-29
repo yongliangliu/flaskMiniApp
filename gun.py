347c5339edd1a684fa18e6dddadb3b5d68bd1dab
yongliangliu <liuyongliang123@gmail.com>
2018-09-29 17:42:34 +0800

    Update gun.py

--- gun.py
+++ gun.py
@@ -24,6 +24,8 @@ chdir = '/home/lyl/pythonService'
 
 daemon = True 
 
+reload = True
+
 #启动的进程数
 workers = multiprocessing.cpu_count() * 2 + 1
 #worker_class = 'gunicorn.workers.ggevent.GeventWorker'
